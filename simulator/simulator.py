759678dc0807b1fe144a8b32540788b18cd2fd95
arunudiyar <130735509+arunudiyar@users.noreply.github.com>
2026-04-08 19:53:08 +0530

    Update simulator.py

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,1 +1,20 @@
+import requests
+import random
+import time
 
+API_URL = "https://fwdjxgz257.execute-api.ap-south-1.amazonaws.com/prod/ingest"
+
+while True:
+
+    payload = {
+        "device_id": "device001",
+        "heart_rate": random.randint(60,120),
+        "bp": random.randint(90,140)
+    }
+
+    response = requests.post(API_URL, json=payload)
+
+    print("Sent:", payload)
+    print("Response:", response.status_code)
+
+    time.sleep(2)
