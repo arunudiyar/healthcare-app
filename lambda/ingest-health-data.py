3eb65d8f7d22fa2b62ac537ba301b4ec1a03037e
arunudiyar <130735509+arunudiyar@users.noreply.github.com>
2026-04-08 19:51:24 +0530

    Update ingest-health-data.py

--- lambda/ingest-health-data.py
+++ lambda/ingest-health-data.py
@@ -1,1 +1,26 @@
+import json
+import boto3
+import base64
+import time
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('health_data')
+
+def lambda_handler(event, context):
+
+    for record in event['Records']:
+
+        payload = base64.b64decode(record['kinesis']['data'])
+        data = json.loads(payload)
+
+        table.put_item(
+            Item={
+                "device_id": data["device_id"],
+                "timestamp": str(int(time.time())),
+                "heart_rate": data["heart_rate"],
+                "bp": data["bp"]
+            }
+        )
+
+    return {"status": "success"}
 
