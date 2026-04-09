f7d1331ce85cdb5fd38960c7513349baeebd8ab6
arunudiyar <130735509+arunudiyar@users.noreply.github.com>
2026-04-09 14:10:52 +0530

    Update simulator.py

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -4,12 +4,18 @@ import time
 
 API_URL = "https://fwdjxgz257.execute-api.ap-south-1.amazonaws.com/prod/ingest"
 
+NUM_DEVICES = 100   # simulate 100 devices locally
+
+device_ids = [f"device{i:05d}" for i in range(1, NUM_DEVICES + 1)]
+
 while True:
 
+    device_id = random.choice(device_ids)
+
     payload = {
-        "device_id": "device001",
-        "heart_rate": random.randint(60,120),
-        "bp": random.randint(90,140)
+        "device_id": device_id,
+        "heart_rate": random.randint(60, 120),
+        "bp": random.randint(90, 140)
     }
 
     response = requests.post(API_URL, json=payload)
@@ -17,4 +23,4 @@ while True:
     print("Sent:", payload)
     print("Response:", response.status_code)
 
-    time.sleep(2)
+    time.sleep(0.2)
